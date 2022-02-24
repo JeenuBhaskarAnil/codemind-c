91f5cb3026e33415cf416c036554504c60da730a
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-02-24 10:44:09 +0530

    #include<stdio.h> int main() {int k,l; scanf("%d",&k); l=k+64;     char i,j;     for(i=l;i>=65;i--)     {         for(j=65;j<=i;j++)         printf("%c ",i);         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
-{
-    int i,j,k;
-    scanf("%d",&k);
-    for(i=1;i<=k;i++)
+{int k,l;
+scanf("%d",&k);
+l=k+64;
+    char i,j;
+    for(i=l;i>=65;i--)
     {
-        for(j=1;j<=i;j++)
-        printf("%d",j);
+        for(j=65;j<=i;j++)
+        printf("%c ",i);
         printf("
 ");
     }
